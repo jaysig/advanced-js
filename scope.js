67ab3be7f622720d6bee3def509800ddd713b0fe
Jaysig <jasontsigmon@gmail.com>
2016-01-24 13:21:14 -0500

    IIFE

--- scope.js
+++ scope.js
@@ -8,5 +8,19 @@
 	//Always want a named function expression ie var name = function nameHandler(){}
 		//Helps with Debugging
 
-//lexical scope - Going up elevator floors
-	//cheated with eval
+//lexical scope - Going up elevator floors, keep looking at higher levels for a declaration 
+	//cheated with eval - makes your code slower
+	//with key - create's a new lexical scope
+
+//IIFE
+var foo = 'foo';
+
+(function(){
+	var foo ="foo2";
+	console.log(foo); //foo2
+})();
+
+console.log(foo);// foo
+//Protects global namespace
+	//Remember to name to protect global namespace
+	
