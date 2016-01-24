ae9f3c01804f781153fcf2c94d2d080790a0c1e9
Jaysig <jasontsigmon@gmail.com>
2016-01-24 14:13:06 -0500

    finished exercise via video

--- day1/exercises/ex1/ex1.js
+++ day1/exercises/ex1/ex1.js
@@ -1,78 +1,78 @@
 (function(global){
-A();
 
-function C() {
-	console.log("OOPS!");
-}
-//Logs oops
-function E(f) {
-	console.log("E");
-	f();
-	var f = F;
-}
-//Logs E Calls F
-function A() {
-	console.log("A");
-	B();
-};
-// Logs A calls B
-var C;
-
-function G() {
-	console.log("G");
-	H();
-
-	function H() {
-		console.log("H");
-		I();
-	};
-}
-//Logs G Calls H, Logs H calls I
-var D = d;
-
-function d() {
-	console.log("D");
-	E();
-}
-
-function I() {
-	console.log("I");
-	J();
-	J();
-}
-
-function B() {
-	console.log("B");
-	C();
-};
-
-var F = function() {
-	console.log("F");
-	G();
-};
-
-var rest = "KLMNOPQRSTUVWXYZ".split("");
-for (var i=0; i<rest.length; i++) {
-	(function(i){
-		// define the current function
-		window[rest[i]] = function() {
-			console.log(rest[i]);
-			if (i < (rest.length-1)) {
-				// TODO: call the next function
-			}
+		function C() {
+			console.log("OOPS!");
+		}
+		//Logs oops
+		function E(f) {
+			console.log("E");
+			f();
+			var f = F;
+		}
+		//Logs E Calls F
+		function A() {
+			console.log("A");
+			B();
 		};
-	})(i);
-}
+		// Logs A calls B
+		var C;
 
-var J = function() {
-	J = function() {
-		console.log("J");
-		K();
-	};
-};
+		function G() {
+			console.log("G");
+			H();
 
-function C() {
-	console.log("C");
-	D();
-};
-})();
+			function H() {
+				console.log("H");
+				I();
+			};
+		}
+		//Logs G Calls H, Logs H calls I
+		var D = d;
+
+		function d() {
+			console.log("D");
+			E(F);
+		}
+
+		function I() {
+			console.log("I");
+			J();
+			J();
+		}
+
+		function B() {
+			console.log("B");
+			C();
+		};
+
+		var F = function() {
+			console.log("F");
+			G();
+		};
+
+		var rest = "KLMNOPQRSTUVWXYZ".split(""), obj={};
+		for (var i=0; i<rest.length; i++) {
+			(function(i){
+				// define the current function
+				obj[rest[i]] = function() {
+					console.log(rest[i]);
+					if (i < (rest.length-1)) {
+						obj[rest[i+1]]();
+					}
+				};
+			})(i);
+		}
+
+		var J = function() {
+			J = function() {
+				console.log("J");
+				obj.K();
+			};
+		};
+
+		function C() {
+			console.log("C");
+			D();
+		};
+		return A;
+})()();
