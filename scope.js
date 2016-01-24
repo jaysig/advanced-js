a520442b0105f363f3e9f1bdcccfca8abbbe47e3
Jaysig <jasontsigmon@gmail.com>
2016-01-24 13:15:58 -0500

    lexical scope

--- scope.js
+++ scope.js
@@ -1,3 +1,12 @@
 //Where to look for things
 
 //Undeclared - unable to find LHS (left hand side) references to a variable
+
+//function declaration - function - first thing in the statement
+
+//Function expression - var name = function 
+	//Always want a named function expression ie var name = function nameHandler(){}
+		//Helps with Debugging
+
+//lexical scope - Going up elevator floors
+	//cheated with eval
