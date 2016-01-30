b0ca3f1fdf4be3b7033df8d34767dc238708f683
Jaysig <jasontsigmon@gmail.com>
2016-01-30 12:04:14 -0600

    callbacks

---
+++ async.js
@@ -0,0 +1,14 @@
+/*Async Patterns
+	Callbacks
+	Generators
+	Promises
+*/
+/* Callbacks
+	Callbacks sucks when there are multiple things needing to fire at once
+	Callback Hell: 
+	Inversion of Control: Handling over control of program to an API or something outside the program
+	Seperate Callbacks: Success and failure callbacks - Does not fix it
+	Error First: If, Else - Does not fix it
+	
+
+*/
